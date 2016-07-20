971cb1d33990560a6f266d6ae8edd54f2f1c1c37
Benjamin <kmrfair@gmail.com>
2016-07-20 14:30:38 +0000

    Windows

---
+++ main.js
@@ -0,0 +1,155 @@
+/*function startAnima(){
+    document.getElementsByClassName("startMenu").className="startMenu";
+}
+*/
+
+/*function startAnima(){
+    document.getElementById("startBtn").className="startMenu";
+}*/
+
+
+//.style.animation = "animationName 2s linear 1";
+
+
+/*
+function startAnima(){
+    document.getElementsByClassName("startMenu").style.animation = "anima 2s";
+}*/
+
+/*
+
+function startAnima(){
+    console.log("test 1");
+    document.getElementsByClassName("startMenu").style.display = "none";
+}
+
+*/
+
+
+
+///////////////////////////////////////////////////////////////
+
+
+/*
+
+
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
+
+      textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(event.dx * event.dx +
+                     event.dy * event.dy)|0) + 'px');
+    }
+  });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+  
+  
+  */
+  
+  
+///////////////////////////////////////////////////////////////
+
+
+
+
+// JAVASCRIPT (jQuery)
+
+// Trigger action when the contexmenu is about to be shown
+$(document).bind("contextmenu", function (event) {
+    
+    // Avoid the real one
+    event.preventDefault();
+    
+    // Show contextmenu
+    $(".custom-menu").finish().toggle(100).
+    
+    // In the right position (the mouse)
+    css({
+        top: event.pageY + "px",
+        left: event.pageX + "px"
+    });
+});
+
+
+// If the document is clicked somewhere
+$(document).bind("mousedown", function (e) {
+    
+    // If the clicked element is not the menu
+    if (!$(e.target).parents(".custom-menu").length > 0) {
+        
+        // Hide it
+        $(".custom-menu").hide(100);
+    }
+});
+
+
+// If the menu element is clicked
+$(".custom-menu li").click(function(){
+    
+    // This is the triggered action name
+    switch($(this).attr("data-action")) {
+        
+        // A case for each action. Your actions here
+        case "first": alert("first"); break;
+        case "second": alert("second"); break;
+        case "third": alert("third"); break;
+    }
+  
+    // Hide it AFTER the action was triggered
+    $(".custom-menu").hide(100);
+  });
+  
+  
+  
+  
+function refreshBrowser() {
+    var ifr = document.getElementsByName('Browser')[0];
+    ifr.src = ifr.src;
+}
+
+  
+  
+  
+  
+/*  
+$('iframe').load( function() {
+    $('iframe').contents().find("head")
+      .append($("<style type='text/css'>  .my-class{display:none;}  </style>"));
+});
+*/
